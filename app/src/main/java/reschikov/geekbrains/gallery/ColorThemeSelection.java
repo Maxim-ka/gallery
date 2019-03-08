d83a4a9d660a711329c9d39ebe91809d4f4262e6
Maxim <Reschikov@list.ru>
2019-03-08 23:28:20 +0800

    Создано две activity Main и ColorThemeSelection. Подобрано 4 цветовых решения. Подготовлен ресурсный файл color с выбранными цветами.
    Подготовлена разметка activity_main, activity_color_theme_selection.

---
+++ app/src/main/java/reschikov/geekbrains/gallery/ColorThemeSelection.java
@@ -0,0 +1,14 @@
+package reschikov.geekbrains.gallery;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ColorThemeSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_theme_selection);
+    }
+}
