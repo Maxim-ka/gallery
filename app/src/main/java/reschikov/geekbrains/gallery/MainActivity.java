e1422db3725a5784658a86fae910d9045b735fd7
Maxim <Reschikov@list.ru>
2019-03-14 12:28:16 +0800

    Создать пару полей TextInputLayout и кнопку "Применить".
    При нажатии на кнопку "Применить" если поля не заполнены - показывать ошибку в незаполненых полях
    Если поле не заполнено и нажата кнопка "Применить", то анимировать с помощью Shake анимации
    Добавить BottomSheet layout с прикрепленной FAB

--- app/src/main/java/reschikov/geekbrains/gallery/MainActivity.java
+++ app/src/main/java/reschikov/geekbrains/gallery/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_master, new FieldsFragment())
+                .commit();
     }
 }
