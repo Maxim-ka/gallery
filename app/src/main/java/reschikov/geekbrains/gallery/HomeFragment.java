38311af268a40d4425447968a851fcc394aec6df
Maxim <Reschikov@list.ru>
2019-03-20 19:13:01 +0800

    Из любого фрагмента открывать другой фрагмент
    Установить анимацию при открытии второго фрагмента
    Сделать анимацию share элемента между этими фрагментами

--- app/src/main/java/reschikov/geekbrains/gallery/HomeFragment.java
+++ app/src/main/java/reschikov/geekbrains/gallery/HomeFragment.java
@@ -5,23 +5,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.android.material.button.MaterialButton;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
     private Counted counted;
+    private Switchable switchable;
+    private AppCompatImageView image;
+
+    public AppCompatImageView getImage() {
+        return image;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        image = view.findViewById(R.id.image_home);
+        ViewCompat.setTransitionName(image, Rule.HOME);
         MaterialButton button = view.findViewById(R.id.button);
         button.setOnClickListener(viewButton -> counted.count());
+        view.setOnClickListener(viewButton -> switchable.toggleFragments(R.id.navigation_notifications));
+
         return view;
     }
 
@@ -29,5 +39,6 @@ public class HomeFragment extends Fragment {
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         counted = (Counted)context;
+        switchable = (Switchable) context;
     }
 }
---
+++ app/src/main/java/reschikov/geekbrains/gallery/Switchable.java
@@ -0,0 +1,5 @@
+package reschikov.geekbrains.gallery;
+
+public interface Switchable {
+    void toggleFragments(int id);
+}
